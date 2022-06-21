0bac861adff8c0f4240c532827deeecec03959d4
lekolawole <lekolawole@gmail.com>
2022-06-21 11:15:44 -0400

    Added styling to WelcomeScreen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import './WelcomeScreen.css';
 
+
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
     (
       <div className="WelcomeScreen">
-        <h1>Welcome to the MeetUp app</h1>
+         <div className="illustration-wrapper">
+          <img className="illustration" src="https://opendoodles.s3-us-west-1.amazonaws.com/rolling.png" alt="illustration" width="300"/>
+          <img className="illustration" src="https://assets.website-files.com/5d5e2ff58f10c53dcffd8683/5d5e305b898356dc76c60d38_chilling.svg" alt="illustration" width="300" />
+        </div>
+        <div className="text-wrapper">
+          <h1>Welcome to MeetUp</h1>
+        <img src='https://github.com/lekolawole/meetup/blob/main/public/meet-app-192.png?raw=true' alt='logo' className="welcome-logo"/>
         <h4>Log in to see upcoming events around the world for full-stack developers</h4>
         <div className="button_cont" align="center">
           <div class="google-btn">
@@ -18,6 +25,7 @@ function WelcomeScreen(props) {
             </div>
         </div>
         <a href="https://lekolawole.github.io/meetup/privacy.html" rel="nofollow noopener">Privacy policy</a>
+        </div>
       </div> )
     : null }
 export default WelcomeScreen;
